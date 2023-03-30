b52008eaeb070b32a30a40447ec950b522362f47
Dennis Demir <dennisjdemir@gmail.com>
2023-03-30 03:11:32 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then((movie) => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.textContent = `${movie.title}`;
+            summary.textContent = `${movie.summary}`;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
